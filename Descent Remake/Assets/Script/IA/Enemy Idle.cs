c04343de7c6d0e4650f9f8b99422bead057031ae
Alberto Tarabella <alberto.tarabella04@gmail.com>
2024-03-09 22:45:39 +0100

    finish ia

--- Descent Remake/Assets/Script/IA/Enemy Idle.cs
+++ Descent Remake/Assets/Script/IA/Enemy Idle.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEngine.EventSystems.EventTrigger;
@@ -9,7 +10,7 @@ public class EnemyIdle : MonoBehaviour{
 
     [SerializeField] RoomEntry entry;
     [SerializeField] RoomExit exit;
-    
+    Rigidbody rb;
 
     bool hasObstacle;
     [SerializeField] float enemySpeed;
@@ -18,24 +19,26 @@ public class EnemyIdle : MonoBehaviour{
     Vector3 startPos;
     private Vector3 target;
     float x, y, z;
+    RaycastHit hit;
 
     private void Start()
     {
+        rb = this.gameObject.GetComponent<Rigidbody>();
         startPos = transform.position;
         x = Random.Range(-4.5f, 4.5f);
         y = Random.Range(0.5f, 2.5f);
         z = Random.Range(-1f, 1f);
         target = new Vector3(x, y, z);
-        hasObstacle = Physics.BoxCast(transform.position,Vector3.one*0.5f,Vector3.zero,Quaternion.identity,1f);
     }
 
     void Update(){
-        hasObstacle = Physics.BoxCast(transform.position, Vector3.one * 0.5f, Vector3.zero, Quaternion.identity, 1f);
-        targetDistance=Vector3.Distance(transform.position,target);
+        hasObstacle = Physics.SphereCast(transform.position, 1f, Vector3.zero, out hit);
+        targetDistance =Vector3.Distance(transform.position,target);
+
         if (hasObstacle==false&&targetDistance>0.01f)
         {
             Vector3 direction=target-transform.position;
-            transform.Translate(direction*Time.deltaTime*enemySpeed,Space.World);
+            rb.velocity = direction*enemySpeed;
         }
         else if(hasObstacle ==true || targetDistance < 0.01f)
         {
--- Descent Remake/Assets/Script/IA/Enemy Movement.cs
+++ Descent Remake/Assets/Script/IA/Enemy Movement.cs
@@ -1,51 +1,100 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
 public class EnemyMovement : MonoBehaviour
 {
 
+    Rigidbody rb;
     [SerializeField] RoomEntry entry;
     [SerializeField] RoomExit exit;
     [SerializeField] PlayerMovement player;
     [SerializeField] float speed;
     [SerializeField] float gap;
+    [SerializeField] float enemyRadius;
 
     bool checkPlayer;
+    RaycastHit hit ;
     bool hasObstacle;
-    RaycastHit hit;
 
     // Start is called before the first frame update
     void Start()
     {
         checkPlayer = false;
+        rb = this.gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        hasObstacle = Physics.SphereCast(transform.position,1f,Vector3.zero,out hit);
         if (checkPlayer)
         {
-            if(hasObstacle==false)
+            transform.LookAt(player.transform.position);
+            if (hasObstacle==false)
             {
+
                 float distance = Vector3.Distance(player.transform.position, transform.position);
                 if (gap<distance)
                 {
                     Vector3 direction = player.transform.position - transform.position;
-                    transform.Translate(direction  * Time.deltaTime * speed, Space.World);
+                    rb.velocity = direction;
                 }
             }
-            else
-            {
-                //transform.Translate(player.transform.position * Time.deltaTime);
-            }
-
         }
 
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Transform obsPos = collision.transform;
+
+        GoBeyondObs(obsPos);
+    }
+
+
+
+    void GoBeyondObs(Transform pos)
+    {
+
+
+
+
+
+
+
+        //bool b1,b2,b3,b4;
+        //Vector3 objCollSize=pos.GetComponent<Collider>().bounds.size;
+        //Vector3 myCollSize = transform.GetComponent<Collider>().bounds.size;
+        //if (transform.position.x+myCollSize.x/2<pos.position.x-objCollSize.x/2)
+        //{
+        //    Vector3 b1Pos=
+        //    b1 = Physics.Raycast();                                          
+        //    //left         
+        //}
+        //else if(transform.position.x - myCollSize.x / 2 > pos.position.x + objCollSize.x / 2)
+        //{
+        //    //right
+        //}else if (transform.position.y - myCollSize.y / 2 > pos.position.y + objCollSize.y / 2)
+        //{
+        //    //up
+        //}
+        //else if (transform.position.y + myCollSize.y / 2 < pos.position.y - objCollSize.y / 2)
+        //{
+        //    //down
+        //}
+        //else if (transform.position.z - myCollSize.z / 2 > pos.position.z + objCollSize.z / 2)
+        //{
+        //    //forward
+        //}
+        //else if (transform.position.z + myCollSize.z / 2 <pos.position.z - objCollSize.z / 2)
+        //{
+        //    //back
+        //}
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
@@ -68,12 +117,10 @@ public class EnemyMovement : MonoBehaviour
     void EntryRoom()
     {
         checkPlayer = true;
-        Debug.Log(checkPlayer);
     }
     void ExitRoom()
     {
         checkPlayer = false;
-        Debug.Log(checkPlayer);
     }
 
 
